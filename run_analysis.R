fa63a97b6cbf89f651e0fe10408257e616e73fa0
Adrian Arhire <arhireadrian@gmail.com>
2015-10-25 18:24:16 +0200

    Finish run_analysis.R script

--- run_analysis.R
+++ run_analysis.R
@@ -1,39 +1,5 @@
----
-title: "run_analysis"
-author: "Adrian Arhire"
-date: "October 25, 2015"
-output: html_document
----
-
-# Project description
-
- The purpose of this project is to demonstrate your ability to collect, work with, and clean a data set. The goal is to prepare tidy data that can be used for later analysis. You will be graded by your peers on a series of yes/no questions related to the project. You will be required to submit: 
-
-1. a tidy data set as described below, 
-2. a link to a Github repository with your script for performing the analysis, and 
-3. a code book that describes the variables, the data, and any transformations or work that you performed to clean up the data called CodeBook.md. You should also include a README.md in the repo with your scripts. This repo explains how all of the scripts work and how they are connected.  
-
- One of the most exciting areas in all of data science right now is wearable computing - see for example this article . Companies like Fitbit, Nike, and Jawbone Up are racing to develop the most advanced algorithms to attract new users. The data linked to from the course website represent data collected from the accelerometers from the Samsung Galaxy S smartphone. A full description is available at the site where the data was obtained: 
-
-http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
-
-Here are the data for the project: 
-
-https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip 
-
- You should create one R script called run_analysis.R that does the following. 
- 
-- Merges the training and the test sets to create one data set.
-- Extracts only the measurements on the mean and standard deviation for each measurement. 
-- Uses descriptive activity names to name the activities in the data set
-- Appropriately labels the data set with descriptive variable names. 
-- From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-
-Good luck!
-
 # Setting up
 
-```{r}
 if (!require("data.table")) {
   install.packages("data.table")
 }
@@ -44,16 +10,14 @@ if (!require("reshape2")) {
 
 library(data.table)
 library(reshape2)
-```
+
 
 # Getting the data
 
-```{r}
 setwd("C:/Work/DataScience/GettingDataProject")
-```
 
-Download the file to "./Data/Dataset.zip" if it doesn't exist
-```{r}
+# Download the file to "./Data/Dataset.zip" if it doesn't exist
+
 downloadUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 dataDirPath <- "./Data"
 zipFilepath <- file.path(dataDirPath,"Dataset.zip")
@@ -64,18 +28,15 @@ if(!file.exists(dataDirPath)) {
     download.file(downloadUrl, zipFilepath)
   }
 }
-```
 
-Unzip the dataset to the "./Data" directory
 
-```{r}
+#Unzip the dataset to the "./Data" directory
+
 unzip(zipFilepath, exdir = dataDirPath)
 datasetPath = file.path(dataDirPath, "UCI HAR Dataset")
-```
 
 # Load the data
 
-```{r}
 subjectTrain <- read.csv(file.path(datasetPath, "train", "subject_train.txt"), sep = "", header = FALSE)
 subjectTest <- read.csv(file.path(datasetPath, "test", "subject_test.txt"), sep = "", header = FALSE)
 trainingSet <- read.csv(file.path(datasetPath, "train", "X_train.txt"), sep = "", header = FALSE)
@@ -85,11 +46,9 @@ testLabels <- read.csv(file.path(datasetPath, "test", "y_test.txt"), sep = "", h
 
 features <- read.csv(file.path(datasetPath, "features.txt"), sep = "", header = FALSE)[,2]
 activityLabels <- read.csv(file.path(datasetPath, "activity_labels.txt"), sep = "", header = FALSE)[,2]
-```
 
 # Merge the data and and descriptive activity names
 
-```{r}
 subject <- rbind(subjectTrain, subjectTest)
 colnames(subject)[1] <- "Subject"
 
@@ -103,26 +62,19 @@ colnames(dataset) <- features
 subject <- cbind(subject, labels, activityLabels[labels[,1]])
 colnames(subject)[3] <- "ActivityName"
 dataset <- cbind(subject, dataset)
-```
 
 # Extract only the mean and standard deviation
 
-```{r}
 filterFeatures <- grepl("mean|std", features)
 dataset <- dataset[,filterFeatures]
-```
 
 # Pivot data
 
-```{r}
 idLabels = c("Subject", "ActivityNumber", "ActivityName")
 dataLabels = setdiff(colnames(dataset), idLabels)
 pivotData = melt(dataset, id = idLabels, measure.vars = dataLabels)
-```
 
 # Tidy data
 
-```{r}
 tidyData   = dcast(pivotData, Subject + ActivityName ~ variable, mean)
 write.table(tidyData, file = "./tidyData.txt")
-```
